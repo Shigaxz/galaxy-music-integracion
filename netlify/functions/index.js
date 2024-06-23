552a7d734c036970ac73e52f3d52f64566e45f31
Shigaxz <va.cabrera@duocuc.cl>
2024-06-23 18:09:10 -0400

    api-rest 2

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -15,38 +15,6 @@ router.get("/promos", function(req, res){
     res.json(promosData());
 });
 
-router.post("/promos", (req, res) => {
-    const newPromo = req.body;
-    newPromo.id = promosData.length + 1;
-    promosData.push(newPromo);
-    res.status(201).json(newPromo);
-});
-
-router.put("/promos/:id", (req, res) => {
-    const { id } = req.params;
-    const updatedPromo = req.body;
-    const promoIndex = promosData.findIndex(promo => promo.id === parseInt(id));
-
-    if (promoIndex !== -1) {
-        promosData[promoIndex] = { ...promosData[promoIndex], ...updatedPromo };
-        res.json(promosData[promoIndex]);
-    } else {
-        res.status(404).json({ message: "Promoción no encontrada" });
-    }
-});
-
-router.delete("/promos/:id", (req, res) => {
-    const { id } = req.params;
-    const promoIndex = promosData.findIndex(promo => promo.id === parseInt(id));
-
-    if (promoIndex !== -1) {
-        const deletedPromo = promosData.splice(promoIndex, 1);
-        res.json(deletedPromo);
-    } else {
-        res.status(404).json({ message: "Promoción no encontrada" });
-    }
-});
-
 app.use('/.netlify/functions/server', router);
 
 export const handler = serverless(app);
--- netlify/functions/promos.js
+++ netlify/functions/promos.js
@@ -1,4 +1,20 @@
 export const promos = () => {
     return [
+        {
+            "id": 76482,
+            "prod": {
+              "id": "MvqoluTjtyZ2U3qWxI0f",
+              "precio": "50000",
+              "categoria": "Instrumento",
+              "descripcion": "una guitarra xd",
+              "nombre": "Guitarra",
+              "imagen": "https://artemusica.cl/web/wp-content/uploads/2024/01/Pack-de-guitarra-acustica-Admira-Alba-Nylon.webp",
+              "stock": "10"
+            },
+            "descuento": "0.25",
+            "precioFinal": 37500,
+            "fechaInic": "xd",
+            "fechaTerm": "xd"
+          }
     ];
   };
