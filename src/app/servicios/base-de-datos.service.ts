f51a5fa9376d2ca0836a1c3bd3d12b2c443f1c05
Valentin Cabrera <137360947+Shigaxz@users.noreply.github.com>
2025-03-05 15:06:51 -0300

    Update base-de-datos.service.ts

--- src/app/servicios/base-de-datos.service.ts
+++ src/app/servicios/base-de-datos.service.ts
@@ -7,13 +7,13 @@ import { initializeApp } from 'firebase/app';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDO5yO5VcktZdjyg38vuqNpoOS4i86-tE4",
-  authDomain: "integracion-d00bf.firebaseapp.com",
-  projectId: "integracion-d00bf",
-  storageBucket: "integracion-d00bf.appspot.com",
-  messagingSenderId: "840054051270",
-  appId: "1:840054051270:web:60004893b8346d0b37e2c7",
-  measurementId: "G-3NG3Q1T6CM"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
